9d2f935c78a5e8c72a5dcc06331df3f6908c9b45
TorientalVortX <83620137+TorientalVortX@users.noreply.github.com>
2022-05-04 03:55:21 -0500

    Add files via upload

---
+++ FinalProject/com/example/src/twitchscrape/scrape.java
@@ -0,0 +1,72 @@
+package twitchscrape;
+
+import java.util.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+
+public class scrape {
+    public void scrape(String a){
+        HashMap<String, Integer>
+        hm = new HashMap<String, Integer>();
+        final String url = a;
+    
+        try {
+        final Document document = Jsoup.connect(url).get();
+
+        // System.out.println(document.outerHtml());
+        
+        for (Element row : document.select(
+            "table.table-condensed.text-center tr")) {
+            if (row.select("td.color-viewers").text().equals("")) {
+                continue;
+            }
+            else {
+                final String tempViews = 
+                       row.select("td.color-viewers").text();
+                final Integer views = Integer.parseInt(tempViews);
+
+                final String name = 
+                       row.select("td a").text();
+               
+                hm.put(name, views);
+                
+            }
+        }
+    }
+    catch (Exception ex) {
+        ex.printStackTrace();
+    }
+
+    Map<String, Integer> hmSorted = sortByValue(hm);
+    for (Map.Entry<String, Integer> i : hmSorted.entrySet()) {
+        System.out.println("Streamer: " + i.getKey() +
+                      ", Avg Viewers: " + i.getValue());
+    }
+}
+
+public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm)
+{
+    // Create a list from elements of HashMap
+    List<Map.Entry<String, Integer> > list =
+           new LinkedList<Map.Entry<String, Integer> >(hm.entrySet());
+
+    // Sort the list
+    Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
+        public int compare(Map.Entry<String, Integer> o1,
+                           Map.Entry<String, Integer> o2)
+        {
+            return (o1.getValue()).compareTo(o2.getValue());
+        }
+    });
+     
+    // put data from sorted list to hashmap
+    HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
+    for (Map.Entry<String, Integer> aa : list) {
+        temp.put(aa.getKey(), aa.getValue());
+    }
+    return temp;
+}
+
+
+
+}
