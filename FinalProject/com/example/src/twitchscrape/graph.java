5dc2c9cd754f516206fefaa51e522deb93f7adc3
TorientalVortX <83620137+TorientalVortX@users.noreply.github.com>
2022-05-10 17:59:25 -0500

    Update graph.java

--- FinalProject/com/example/src/twitchscrape/graph.java
+++ FinalProject/com/example/src/twitchscrape/graph.java
@@ -1,55 +1,81 @@
 package twitchscrape;
 
 import java.util.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
 
-public class graph<T> {
-    private Map<T, List<T>> graph = new HashMap<>();
+public class scrape {
+    public void scrape(String a){
+        HashMap<String, String>
+        hm = new HashMap<String, String>();
+        final String url = a;
+    
+        try {
+        final Document document = Jsoup.connect(url).get();
 
-    public void addEdge(String source, T destination, boolean biDirectional) {
-        if (!graph.containsKey(source)) {
-            addVertex((T) source);
-        }
-
-        if (!graph.containsKey(destination)) {
-            addVertex(destination);
-        }
-
-        graph.get(source).add(destination);
-        if(biDirectional == true) {
-            graph.get(destination).add((T) source);
-        }
-    }
-
-    public void hasVertex(T vertex) {
-        if(graph.containsKey(vertex)) {
-            System.out.println("The Graph contains " + vertex + " as a vertex");
-        }else {
-            System.out.println("The Graph does not contain " + vertex + " as a vertex");
-        }
-    }
-
-    public void hasEdge(T source, T destination) {
-        if(graph.get(source).contains(destination)) {
-            System.out.println("The Graph has an edge between " + source + " and " + destination);
-        }else {
-            System.out.println("The Graph has no edge between " + source + " and " + destination);
-        }
-    }
-
-    public String printGraph() {
-        StringBuilder builder = new StringBuilder();
-
-        for(T vertex : graph.keySet()) {
-            builder.append("\n" + vertex.toString() + "->");
-            for(T node: graph.get(vertex)) {
-                builder.append(node.toString() + " ");
+        // System.out.println(document.outerHtml());
+        
+        for (Element row : document.select(
+            "table.table-condensed.text-center tr")) {
+            if (row.select("td div.meta").first().text().equals("")) {
+                continue;
             }
-            builder.append("\n");
-        }
-        return builder.toString();
-    }
+            else {
+                final String tempViews = 
+                       row.select("td span.viewers-value").text();
+                final Integer views = Integer.parseInt(tempViews);
 
-    private void addVertex(T vertex) {
-        graph.put(vertex, new LinkedList<T>());
+                final String name = 
+                       row.select("td a").text();
+
+                final String game = 
+                       row.select("td div.meta").first().text();
+               
+                hm.put(name, game);
+                
+            }
+        }
     }
+    catch (Exception ex) {
+        ex.printStackTrace();
+    }
+    System.out.println(hm);
+
+    graph<Integer> graphObject = new graph<>();
+    Map<String, String> hmSorted = sortByValue(hm);
+    for (Map.Entry<String, String> i : hmSorted.entrySet()) {
+        graphObject.addEdge(i.getValue(), i.getKey(),  true);
+        System.out.println("Streamer: " + i.getKey() +
+                      ", Game: " + i.getValue());
+                     
+    }
+    System.out.println("\nGraph:\n"
+        + graphObject.printGraph());
+}
+
+public static HashMap<String, String> sortByValue(HashMap<String, String> hm)
+{
+    // Create a list from elements of HashMap
+    List<Map.Entry<String, String> > list =
+           new LinkedList<Map.Entry<String, String> >(hm.entrySet());
+
+    // Sort the list
+    Collections.sort(list, new Comparator<Map.Entry<String, String> >() {
+        public int compare(Map.Entry<String, String> o1,
+                           Map.Entry<String, String> o2)
+        {
+            return (o1.getValue()).compareTo(o2.getValue());
+        }
+    });
+     
+    // put data from sorted list to hashmap
+    HashMap<String, String> temp = new LinkedHashMap<String, String>();
+    for (Map.Entry<String, String> aa : list) {
+        temp.put(aa.getKey(), aa.getValue());
+    }
+    return temp;
+}
+
+
+
 }
