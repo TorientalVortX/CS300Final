dc92138804888919b5d2a4492c425219403af61b
TorientalVortX <83620137+TorientalVortX@users.noreply.github.com>
2022-05-10 17:15:36 -0500

    Update main.java

--- FinalProject/com/example/src/twitchscrape/main.java
+++ FinalProject/com/example/src/twitchscrape/main.java
@@ -3,6 +3,6 @@ package twitchscrape;
 public class main {
     public static void main(String[] args) {
         scrape twitch = new scrape();
-        twitch.scrape("http://twitchtracker.com/channels/ranking");
+        twitch.scrape("https://twitchtracker.com/channels/live");
     }
 }
