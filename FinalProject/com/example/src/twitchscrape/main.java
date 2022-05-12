974b6fdb9b0639efdf95756ae6c5068c2dfbd38c
TorientalVortX <83620137+TorientalVortX@users.noreply.github.com>
2022-05-11 19:27:20 -0500

    Delete main.java

--- FinalProject/com/example/src/twitchscrape/main.java
+++
@@ -1,8 +0,0 @@
-package twitchscrape;
-
-public class main {
-    public static void main(String[] args) {
-        scrape twitch = new scrape();
-        twitch.scrape("https://twitchtracker.com/channels/live");
-    }
-}
